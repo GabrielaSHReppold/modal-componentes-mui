587c2b58073f6147eba4fe866e911e7acaf2e113
Gabriela Reppold <reppoldgabriela@gmail.com>
2024-11-30 17:19:30 -0300

    configs e routes

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+  return (
+    <div>
+      <h1>404 - Página não encontrada</h1>
+      <p>Desculpe, a página que você está tentando acessar não existe.</p>
+    </div>
+  );
+}
